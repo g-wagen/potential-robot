03b414b291a2156f383e9d51df2329e936f15080
g-wagen <>
2022-01-29 17:30:16 +0100

    Add scraper python files. Add dotenv to requirements. Change package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='potential-robot',
-      version="1.0",
+      version="0.0.1",
       description="Project Description",
       packages=find_packages(),
       install_requires=requirements,
